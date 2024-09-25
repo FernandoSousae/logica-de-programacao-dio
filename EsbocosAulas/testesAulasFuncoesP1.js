073744117b33324c8da2515dba2ea2071877672c
FernandoSousae <fer.pe.sou.86@gmail.com>
2024-09-25 06:28:31 -0300

    esboços das aulas de Lógica

---
+++ EsbocosAulas/testesAulasFuncoesP1.js
@@ -0,0 +1,31 @@
+/*torrar()
+
+function torrar(){
+    console.log(`Torrando Pão`)
+    injetarPao()
+}
+
+function injetarPao(){
+    console.log(`Preparando para injetar o pão.`)
+    console.log(`Finalizado`)
+}*/
+
+mainSaveData()
+
+function mainSaveData(){
+    getData()
+    checkValues()
+    sendToDatabase()
+}
+
+function getData(){
+    console.log(`Pegando dados do usuário...`)
+}
+
+function checkValues(){
+    console.log(`validando dados...`)
+}
+
+function sendToDatabase(){
+    console.log(`Cadastrando os dados!!!`)
+}
