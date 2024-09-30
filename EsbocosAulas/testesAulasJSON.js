80bb238d053575944bf6397c6e513806e974bea5
FernandoSousae <fer.pe.sou.86@gmail.com>
2024-09-30 06:17:17 -0300

    Update testesAulasJSON.js

--- EsbocosAulas/testesAulasJSON.js
+++ EsbocosAulas/testesAulasJSON.js
@@ -1,22 +1,22 @@
-let invoice = {
-    name:"Fernando",
+let invoice ={
+    name: 'Fernando',
     age: 38,
-    products: {
-        0: ["mouse 2xwm", 29.99],
-        1: ["teclado mecânico", 129.99],
-        2: ["Monitor", 899.99]
+    products:{
+        0: ["Mouse 2xwm", 29.99],
+        1: ["Tecledo mecânico", 129.90],
+        2: ["Monitor", 899.90]
     }
 }
 
 generateInvoice(invoice)
 
 function generateInvoice(invoice){
-    console.log(`O comprador é ${invoice.name}`)
-    console.log(`A idade é ${invoice.age}`)
-
+    console.log(`O comprador é ${invoice.name}.`)
+    console.log(`A idade é ${invoice.age}.`)
+    
     for(let index in invoice.products){
-        let [productName, productPrice] = invoice[index]
-        console.log(`Os produtos são ${invoice.products}`)
+        let [productName, productPrice] = invoice.products[index]
+        console.log(` - ${productName}: R$ ${productPrice}`)
     }
-    console.log(`O produto é ${invoice.products[0]}`)
+
 }
